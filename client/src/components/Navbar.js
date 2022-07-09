4e1554e80104b0bfbb64f27b662c2ada110c06ec
Tauhoo <tauhoo_ice@hotmail.com>
2022-07-09 16:16:26 +0700

    create logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import styled from "styled-components"
+import Logo from "./Logo"
 
 const Container = styled.div`
     width: 100%;
-    height: 69px;
     border: 1px solid rgb(235, 237, 240);
+    padding: 10px 20px;
 `
 
 function Navbar() {
-    return <Container></Container>
+    return <Container>
+        <Logo height="40" width="40" />
+    </Container>
 }
 
 export default Navbar
